f2be60cc339115b481c706381b310a4a0098702d
zxVIE Zawadzki <zygmunt.zawadzki.ext@boehringer-ingelheim.com>
2021-07-09 11:15:10 +0200

    Simulations summary

--- inst/projects/models-analysis.R
+++ inst/projects/models-analysis.R
@@ -23,4 +23,32 @@ result %>% nrow()
 library(ggplot2)
 
 ggplot(result) + geom_boxplot(aes(Models, Accuracy, color = FeatureAlgo)) + 
+  facet_wrap(c("hash", "hallmarkGeneSet", "FeatureNumbers"))
+
+
+sumAcc <- result %>%
+  group_by(Models, FeatureAlgo, FeatureNumbers, hash, hallmarkGeneSet) %>%
+  summarise(
+    MEAN = mean(Accuracy),
+    Q05  = quantile(Accuracy, 0.05),
+    Q95  = quantile(Accuracy, 0.05),
+    MIN  = min(Accuracy),
+    MAX  = max(Accuracy)
+  ) %>% ungroup()
+
+res <- sumAcc %>% mutate(FeatureNumbers = as.numeric(as.factor(FeatureNumbers)))
+
+ggplot(res) + 
+  geom_line(aes(FeatureNumbers, Q05, color = FeatureAlgo)) + 
   facet_wrap(c("hash", "hallmarkGeneSet"))
+
+
+
+
+p
+library(plotly)
+ggplotly(p)
+
+
+#
+#unlink(file.path("~/ml-simulation/", setdiff(basename(allModels), basename(allPaths))))
