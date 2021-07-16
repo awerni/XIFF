7c5f24ea9b7239f25d6e14b1cd4474ba3163814e
zxVIE Zawadzki <zygmunt.zawadzki.ext@boehringer-ingelheim.com>
2021-07-16 12:05:44 +0200

    update simulation results

--- inst/projects/models-analysis.R
+++ inst/projects/models-analysis.R
@@ -18,37 +18,4 @@ readMetrics <- function(path) {
 
 result <- purrr::map_dfr(allModels, readMetrics)
 
-result %>% nrow()
-
-library(ggplot2)
-
-ggplot(result) + geom_boxplot(aes(Models, Accuracy, color = FeatureAlgo)) + 
-  facet_wrap(c("hash", "hallmarkGeneSet", "FeatureNumbers"))
-
-
-sumAcc <- result %>%
-  group_by(Models, FeatureAlgo, FeatureNumbers, hash, hallmarkGeneSet) %>%
-  summarise(
-    MEAN = mean(Accuracy),
-    Q05  = quantile(Accuracy, 0.05),
-    Q95  = quantile(Accuracy, 0.05),
-    MIN  = min(Accuracy),
-    MAX  = max(Accuracy)
-  ) %>% ungroup()
-
-res <- sumAcc %>% mutate(FeatureNumbers = as.numeric(as.factor(FeatureNumbers)))
-
-ggplot(res) + 
-  geom_line(aes(FeatureNumbers, Q05, color = FeatureAlgo)) + 
-  facet_wrap(c("hash", "hallmarkGeneSet"))
-
-
-
-
-p
-library(plotly)
-ggplotly(p)
-
-
-#
-#unlink(file.path("~/ml-simulation/", setdiff(basename(allModels), basename(allPaths))))
+saveRDS(result, "inst/projects/simulation-result-v2-2021-07-16.rds")
