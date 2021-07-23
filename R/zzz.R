df5cfab9b92b8be96da07603cd1d67871a21dabe
zxVIE Zawadzki <zygmunt.zawadzki.ext@boehringer-ingelheim.com>
2021-07-23 09:06:33 +0200

    hack for registering the roboto font

--- R/zzz.R
+++ R/zzz.R
@@ -28,7 +28,18 @@
       bolditalic = list(getRobotoDir("bolditalic"), 0)
     )
   }
-
+  
+  if(!"Roboto" %in% names(pdfFonts())) {
+    # solve the warning regarding missing font in pdf device
+    # it's a hack, but other solution didn't work e.g. registering
+    # the fonts using afm files (Type1Font - function). They make the
+    # font visible for cairo_pdf device but not the pdf device
+    z <- pdfFonts()[["Helvetica"]]
+    z$family <- "Roboto"
+    pdfFonts(Roboto = z)
+    postscriptFonts(Roboto = z)
+  }
+  
   t <- theme_get()
   t$text$family <- "Roboto"
   theme_set(t)
